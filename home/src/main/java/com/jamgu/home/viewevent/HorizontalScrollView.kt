a0fe35f4c3e27f2025df352d618bb509e2970a9a
jamgudev <826630153@qq.com>
2022-02-27 14:44:45 +0800

    feat: 上传同向、混合滑动冲突场景处理源码。

--- home/src/main/java/com/jamgu/home/viewevent/HorizontalScrollView.kt
+++ home/src/main/java/com/jamgu/home/viewevent/HorizontalScrollView.kt
@@ -5,9 +5,13 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.VelocityTracker
 import android.view.ViewGroup
+import android.widget.ScrollView
 import android.widget.Scroller
 import androidx.core.view.children
 import androidx.core.view.isVisible
+import androidx.core.view.marginRight
+import androidx.core.view.marginStart
+import androidx.recyclerview.widget.RecyclerView
 import com.jamgu.common.util.log.JLog
 import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
@@ -17,9 +21,9 @@ private const val TAG = "HorizontalScrollView"
 /**
  * Created by jamgu on 2022/02/18
  *
- * 解决父-左右，子上下滑动冲突场景
+ * 解决父-左右，子上下滑动冲突（异向）、同向以及混合滑动冲突场景
  */
-class HorizontalScrollView: ViewGroup {
+class HorizontalScrollView: ViewGroup, IOverScroll {
 
     private var mLastX = 0.0f
     private var mLastY = 0.0f
@@ -27,6 +31,10 @@ class HorizontalScrollView: ViewGroup {
     private var mLastInterceptY = 0.0f
     private var mTracker = VelocityTracker.obtain()
     private var mScroller = Scroller(context)
+    // 记录当前显示子 View 的索引
+    private var mChildCurIdx = 0
+    // 记录当前滑动的方向
+    private var mTouchDirection = DIRECTION_NONE
 
     constructor(context: Context) : super(context) {
         init()
@@ -58,7 +66,15 @@ class HorizontalScrollView: ViewGroup {
             MotionEvent.ACTION_MOVE -> {
                 val dX = (x - mLastInterceptX).absoluteValue
                 val dY = (y - mLastInterceptY).absoluteValue
-                if (dX > dY) {
+                mTouchDirection = if (dX > dY) {
+                    if (x - mLastX >= 0) DIRECTION_RIGHT
+                    else DIRECTION_LEFT
+                } else {
+                    if (y - mLastY >= 0) {
+                        DIRECTION_DOWN
+                    } else DIRECTION_UP
+                }
+                if (dX > dY || isUDOverScroll(ev)) {
                     intercepted = true
                 }
             }
@@ -83,48 +99,60 @@ class HorizontalScrollView: ViewGroup {
 
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
+                if (!mScroller.isFinished) {
+                    mScroller.abortAnimation()
+                }
             }
             MotionEvent.ACTION_MOVE -> {
-                var deltaX = x - mLastX
+                val deltaX = x - mLastX
                 val deltaY = y - mLastY
-                val childCurIdx = scrollX / child.width
-                val scrollPercent = ((scrollX * 1.0f  % child.width) / child.width).absoluteValue
-                JLog.d(TAG, "scrollPercent = $scrollPercent, childCurIdx = $childCurIdx, deltaX = $deltaX, scrollX = $scrollX")
-                if (isOverScrolled()) {
-                    scrollBy(-deltaX.roundToInt() / 2, 0)
+                if (isTouchDirectionHorizontal(mTouchDirection)) {
+                    if (isLROverScroll(event)) {
+                        scrollBy(-deltaX.roundToInt() / 2, 0)
+                    } else {
+                        scrollBy(-deltaX.roundToInt(), 0)
+                    }
                 } else {
-                    scrollBy(-deltaX.roundToInt(), 0)
+                    scrollBy(0, -deltaY.roundToInt() / 2)
                 }
             }
             MotionEvent.ACTION_UP -> {
-                mTracker.computeCurrentVelocity(1000)
-                val xVelocity = mTracker.xVelocity
-                child.let {
-                    val childWidth = it.width
-                    var childIdx = (scrollX / childWidth)
-                    childIdx = if (xVelocity.absoluteValue >= 50) {
-                        if (xVelocity > 0) childIdx else childIdx + 1
-                    } else {
-                        // 在用户抬起手指时，当前的childIdx已经-1了，所以不用再减1了
-                        (scrollX + childWidth / 2) / childWidth
-                    }
-                    childIdx = childIdx.coerceAtLeast(0).coerceAtMost(childCount - 1)
-                    val dx = childIdx * childWidth - scrollX
+                if (isTouchDirectionHorizontal(mTouchDirection)) {
+                    mTracker.computeCurrentVelocity(1000)
+                    val xVelocity = mTracker.xVelocity
+                    child.let {
+                        val childWidth = it.width
+                        var childIdx = (scrollX / childWidth)
+                        childIdx = if (xVelocity.absoluteValue >= 100) {
+                            if (xVelocity > 0) childIdx else childIdx + 1
+                        } else {
+                            // 在用户抬起手指时，当前的childIdx已经-1了，所以不用再减1了
+                            (scrollX + childWidth / 2) / childWidth
+                        }
+                        childIdx = childIdx.coerceAtLeast(0).coerceAtMost(childCount - 1)
+                        val dx = childIdx * childWidth - scrollX
 
-                    smoothScrollBy(dx, 0)
-                    mTracker.clear()
+                        smoothScrollBy(dx, 0)
+                        mTracker.clear()
+
+                        mChildCurIdx = childIdx
+                    }
+                } else {
+                    smoothScrollBy(0, -scrollY)
                 }
+
+                mTouchDirection = DIRECTION_NONE
             }
         }
 
         mLastX = x
         mLastY = y
 
-        return true
+        return super.onTouchEvent(event)
     }
 
     private fun smoothScrollBy(dx: Int, dy: Int) {
-        mScroller.startScroll(scrollX, 0, dx, 0, 500)
+        mScroller.startScroll(scrollX, scrollY, dx, dy, 500)
         invalidate()
     }
 
@@ -136,8 +164,8 @@ class HorizontalScrollView: ViewGroup {
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        var finalWidth = 0
-        var finalHeight = 0
+        var finalWidth = paddingLeft + paddingRight
+        var finalHeight = paddingTop + paddingBottom
         measureChildren(widthMeasureSpec, heightMeasureSpec)
         if (childCount > 0) {
             children.forEach { view ->
@@ -156,13 +184,12 @@ class HorizontalScrollView: ViewGroup {
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         if (childCount > 0) {
-            var childLeft = 0
+            var childLeft = paddingLeft
+            var childTop = paddingTop
+            var childBottom = paddingBottom
             children.forEach { view ->
                 if (! view.isVisible) return@forEach
 
-                childLeft += paddingLeft
-                var childTop = paddingTop
-                var childBottom = paddingBottom
                 val measuredWidth = view.measuredWidth
                 val measuredHeight = view.measuredHeight
                 val lp = view.layoutParams as? MarginLayoutParams ?: return
@@ -176,15 +203,35 @@ class HorizontalScrollView: ViewGroup {
         }
     }
 
-    private fun isOverScrolled(): Boolean {
-        var wholeWidth = 0L
-        children.forEach {
-            wholeWidth += it.width
-        }
-        return scrollX < 0 || scaleX > (wholeWidth + paddingRight + paddingLeft - width)
-    }
-
     override fun generateLayoutParams(attrs: AttributeSet?): LayoutParams {
         return MarginLayoutParams(context, attrs)
+    }
+
+    override fun isUDOverScroll(ev: MotionEvent?): Boolean {
+        val childAt = getChildAt(mChildCurIdx) ?: return false
+        var isUDOverScroll = false
+
+        when(childAt) {
+            is RecyclerView -> {
+                ev?.let {
+                    isUDOverScroll = isRecyclerViewAbout2OverScroll(childAt, mTouchDirection)
+                }
+            }
+
+            is ScrollView -> {
+
+            }
+        }
+
+        JLog.d(TAG, "isUDOverScroll = $isUDOverScroll")
+        return isUDOverScroll
+    }
+
+    override fun isLROverScroll(ev: MotionEvent?): Boolean {
+        var wholeWidth = 0L
+        children.forEach {
+            wholeWidth += it.width + it.marginStart + it.marginRight
+        }
+        return scrollX < 0 || scaleX > (wholeWidth + paddingRight + paddingLeft - width)
     }
 }
